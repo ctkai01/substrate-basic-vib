912d591fa6f7301a67df8b19c2e2fe474af75ba6
ctkai01 <lainam113201@gmail.com>
2022-07-05 13:29:18 +0700

    first commit

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,20 +4,16 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
-
 #[cfg(test)]
 mod mock;
 
 #[cfg(test)]
 mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+	use frame_support::{pallet_prelude::*};
+	use frame_system::{pallet_prelude::*};
+	use frame_support::sp_runtime::print;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -38,6 +34,9 @@ pub mod pallet {
 	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
+	#[pallet::storage]
+	pub type Number<T:Config> = StorageMap<_, Blake2_128Concat, T::AccountId, u32, ValueQuery, >;
+
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
@@ -46,6 +45,7 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+		RemoveStored(T::AccountId),
 	}
 
 	// Errors inform users that something went wrong.
@@ -71,12 +71,35 @@ pub mod pallet {
 			// https://docs.substrate.io/v3/runtime/origins
 			let who = ensure_signed(origin)?;
 
-			// Update storage.
+			// Update storage.	
 			<Something<T>>::put(something);
 
 			// Emit an event.
 			Self::deposit_event(Event::SomethingStored(something, who));
 			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn put_number(origin: OriginFor<T>, number: u32) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			
+			<Number<T>>::insert(who.clone(), number);
+
+			Self::deposit_event(Event::SomethingStored(number, who));
+
+			Ok(())
+		}
+
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn clear_number(origin: OriginFor<T>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			<Number<T>>::remove(&who);
+
+			Self::deposit_event(Event::RemoveStored(who));
+
 			Ok(())
 		}
 
